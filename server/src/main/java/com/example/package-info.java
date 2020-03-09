a6bff27d98a89e9be83af5e1ebfa7811620286d2
root <pbilbaoc@gmail.com>
2020-03-09 11:58:38 -0300

    reparacion checkstyle Warnings

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 /**
  * This is a package name
  */
 
 package com.example;
 
-=======
-/** This is a package name
-*/
-package com.example;
->>>>>>> ba9efe38e6a7695fc119400fa6e094cf6fd31df0
