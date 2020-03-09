8f3c17b31dcb94fb826470d9ffa7af34d110136a
root <pbilbaoc@gmail.com>
2020-03-09 11:56:36 -0300

    reparacion checkstyle Warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,19 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-<<<<<<< HEAD
-  /**
+/**
  * @param someone the name 
  * @return greeting string
  */
 
-=======
-//**
-  * @param someone the name of a person
-  * @return greeting string  
-  */
-    
->>>>>>> ba9efe38e6a7695fc119400fa6e094cf6fd31df0
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
